6ad4d30d23866445e99ec42dca4120fd34e7233c
ivan <ivan@nextail.co>
2018-10-26 13:14:40 +0200

    Using remote state

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,3 +1,7 @@
 terraform {
-  backend "local" {}
+  backend "s3" {
+    bucket         = "ivan-backend-terraform-workshop"
+    key            = "terraform.tfstate"
+    dynamodb_table = "ivan-backend-terraform-workshop"
+  }
 }
